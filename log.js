8b35d6621d1cd61a3ab9dd1f967b370548a805c7
Will <667@onezerohosting.com>
2020-11-29 10:08:01 -0800

    Overriding console.log not such a good idea.

--- log.js
+++ log.js
@@ -1,11 +1,9 @@
-const log = console.log
-
 const yellow = '\x1b[33m',
     off = '\x1b[0m';
 const name = `[${yellow}ngrok-dns${off}]`
 
 function logger(message) {
-    log(`${name} ${message}`)
+    console.log(`${name} ${message}`)
 }
 
 module.exports = logger
